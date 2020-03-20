3d74903e27d7ab5eab21fb90c604a50ddb0d8226
Thatikonda <760352@cognizant.com>
2020-03-20 18:09:16 +0530

    migration

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -9,7 +9,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class PalTrackerApplication {
@@ -18,18 +18,26 @@ public class PalTrackerApplication {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
 
+    /* @Bean
+     TimeEntryRepository timeEntryRepository(){
+         return new InMemoryTimeEntryRepository();
+     }*/
+    /*@Bean
+    TimeEntryRepository JdbctimeEntryRepository(DataSource dataSource) {
+        return new InMemoryTimeEntryRepository(dataSource);
+    }*/
     @Bean
-    TimeEntryRepository timeEntryRepository(){
-        return new InMemoryTimeEntryRepository();
+    TimeEntryRepository timeEntryRepository(DataSource dataSource){
+        return new JdbcTimeEntryRepository(dataSource);
     }
 
     @Bean
-    public ObjectMapper jsonObjectMapper() {
-        return Jackson2ObjectMapperBuilder.json()
-                .serializationInclusion(JsonInclude.Include.NON_NULL) // Don’t include null values
-                .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS) //ISODate
-                .modules(new JavaTimeModule())
-                .build();
+        public ObjectMapper jsonObjectMapper(){
+            return Jackson2ObjectMapperBuilder.json()
+                    .serializationInclusion(JsonInclude.Include.NON_NULL) // Don’t include null values
+                    .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS) //ISODate
+                    .modules(new JavaTimeModule())
+                    .build();
+        }
     }
 
-}
